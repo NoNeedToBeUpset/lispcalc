feee6503e75b896eef51defcec0a0607518650b1
NoNeedToBeUpset <noneedtobeupset@cock.li>
2014-04-13 18:08:21 +0200

    added initsym.h

--- config.mk
+++ config.mk
@@ -2,5 +2,5 @@ CC=cc
 CFLAGS=-Wall -g
 LDFLAGS=-lm
 EXECUTABLE=lispcalc
-HEADERS=builtins.h lispcalc.h symbol.h types.h util.h
+HEADERS=builtins.h initsym.h lispcalc.h symbol.h types.h util.h
 SOURCES=builtins.c main.c symbol.c util.c
