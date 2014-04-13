b1610d5f538934274d8186be5775f2401417f083
NoNeedToBeUpset <noneedtobeupset@cock.li>
2014-04-13 18:07:27 +0200

    now has all the four basic arithmetic operations

--- builtins.c
+++ builtins.c
@@ -5,7 +5,52 @@
 #include "util.h"
 #include "builtins.h"
 
-static int addtwo(struct value *val, const struct value *addme);
+static int _add(struct value *val, const struct value *addme);
+static int _div(struct value *v1, const struct value *v2);
+static int _mul(struct value *v1, const struct value *v2);
+static int _sub(struct value *v1, const struct value *v2);
+
+static int reduce(struct value *val, const struct value *val2,
+		int(*reducefunc)(struct value*, const struct value*));
+
+/* reduce two values to one, using the arithmetic function reducefunc
+ * the cool thing: makes sure the types are compatible */
+static int
+reduce(struct value *val, const struct value *val2,
+	int(*reducefunc)(struct value*, const struct value*))
+{
+	int retv;
+	struct value *fv = NULL;
+
+	if(val->valtype != val2->valtype){
+		/* first off make sure we are not trying to do something
+		 * utterly retarded */
+		if(!NUMERIC(val2))
+			return 0;
+
+		/* now we can work with the assumption that
+		 * t(val) ^ t(val2) == 1,
+		 * which means we convert the int to float */
+		if(val->valtype == ival){
+			val->valtype = fval;
+			val->fval = (double)val->ival;
+		}
+		else if(val2->valtype == ival){
+			fv = xmalloc(sizeof(struct value));
+			fv->valtype = fval;
+			fv->fval = (double)val2->ival;
+		}
+		else DEVELOPERPLS("THE TYPES ARE MESSED UP");
+
+	}
+	retv = reducefunc(val, fv ? fv : val2);
+
+	/* clean up if we allocated memory */
+	if(fv)
+		free(fv);
+
+	return retv;
+}
 
 /* this adds an arbitrary number of values together,
  * regardless of them being ints/floats
@@ -18,61 +63,139 @@ builtin_add(struct args *args)
 
 	val = xmalloc(sizeof(struct value));
 
-	val->valtype = args->val.valtype;
-	if(val->valtype == ival)
-		val->ival = args->val.ival;
-
-	else if(val->valtype == fval)
-		val->fval = args->val.fval;
-
-	/* what the shit this guy is trying to add a function to a string
-	 * or something */
-	else goto add_typeerr;
+	/* start with a known good value (also the empty sum, which is neat)
+	 * this will be converted to float if needed */
+	val->valtype = ival;
+	val->ival = 0;
 
 	/* add ALL the arguments */
-	for(args = args->next; args; args = args->next){
+	for(; args; args = args->next){
 		/* the retard is at it again */
-		if(!addtwo(val, &args->val))
-			goto add_typeerr;
+		if(!reduce(val, &args->val, _add)){
+			free(val);
+			return mkretrn(1, genericerror(err_type,
+					"adding those make no sense"));
+		}
 	}
 
 	/* return with great success */
 	return mkretrn(1, val);
-
-	/* oh noes, someone tried printf+"potato" */
-add_typeerr:
-	free(val);
-	return mkretrn(1, genericerror(err_type,
-				"adding those make no sense"));
 }
 
-/* helper for builtin_add, val+addme -> val, with conversion if necessary */
-static int
-addtwo(struct value *val, const struct value *addme)
+struct retrn*
+builtin_div(struct args *args)
 {
-	/* make sure the types match */
-	if(val->valtype != addme->valtype){
-		/* first off make sure we are not trying to do something
-		 * utterly retarded */
-		if(!NUMERIC(addme))
-			return 0;
+	struct value *val;
 
-		/* now we can work with the assumption that
-		 * t(val) ^ t(addme) == 1,
-		 * which means we convert the int to float */
-		else if(val->valtype == ival){
-			val->valtype = fval;
-			val->fval = (double)val->ival;	/* is this safe? */
-			val->fval += addme->fval;	/* addition complete */
-		}
-		else val->fval += (double)addme->ival;
-		return 1;
+	/* division only operates on doubles */
+	val = xmalloc(sizeof(struct value));
+	val->valtype = fval;
+	if(args->val.valtype == fval)
+		val->fval = args->val.fval;
+
+	else if(args->val.valtype == ival)
+		val->fval = (double)args->val.ival;
+
+	else goto div_typeerr;
+
+	for(args = args->next; args; args = args->next){
+		if(!reduce(val, &args->val, _div))
+			goto div_typeerr;
 	}
 
+	return mkretrn(1, val);
+
+div_typeerr:
+	free(val);
+	return mkretrn(1, genericerror(err_type,
+			"that makes no sense"));
+}
+
+struct retrn*
+builtin_mul(struct args *args)
+{
+	struct value *val;
+
+	/* start with a known good value, the empty product */
+	val = xmalloc(sizeof(struct value));
+	val->valtype = ival;
+	val->ival = 1;
+
+	for(; args; args = args->next){
+		if(!reduce(val, &args->val, _mul)){
+			free(val);
+			return mkretrn(1, genericerror(err_type,
+					"multiplying those makes no sense"));
+		}
+	}
+
+	return mkretrn(1, val);
+}
+
+struct retrn*
+builtin_sub(struct args *args)
+{
+	struct value *val;
+
+	if(!NUMERIC(&args->val))
+		goto sub_typeerror;
+
+	val = xmalloc(sizeof(struct value));
+	setval(val, &args->val);
+
+	for(args = args->next; args; args = args->next){
+		if(!reduce(val, &args->val, _sub))
+			goto sub_typeerror;
+	}
+
+	return mkretrn(1, val);
+
+sub_typeerror:
+	free(val);
+	return mkretrn(1, genericerror(err_type,
+			"that subtraction makes no sense"));
+}
+
+/* to be passed as reducefunc for reduce
+ * we can make lots of assumptions, reduce fixed everything */
+static int
+_add(struct value *val, const struct value *addme)
+{
 	if(val->valtype == ival)
 		val->ival += addme->ival;
 
 	else val->fval += addme->fval;
+
+	return 1;
+}
+
+static int
+_div(struct value *v1, const struct value *v2)
+{
+	/* div is only done with floats */
+	v1->fval /= v2->fval;
+
+	return 1;
+}
+
+static int
+_mul(struct value *v1, const struct value *v2)
+{
+	if(v1->valtype == ival)
+		v1->ival *= v2->ival;
+
+	else v1->fval *= v2->fval;
+
+	return 1;
+}
+
+static int
+_sub(struct value *v1, const struct value *v2)
+{
+	if(v1->valtype == ival)
+		v1->ival -= v2->ival;
+
+	else v1->fval -= v2->fval;
 
 	return 1;
 }
--- builtins.h
+++ builtins.h
@@ -5,8 +5,14 @@
 
 #define NUMERIC(val) ((val)->valtype == ival || (val)->valtype == fval)
 
-struct retrn* builtin_add(struct args *args);
+/* output, debugging and data inspection */
 struct retrn* builtin_dump_symtbl(struct args *args);
 struct retrn* builtin_println(struct args *args);
+
+/* arithmetic */
+struct retrn* builtin_add(struct args *args);
+struct retrn* builtin_div(struct args *args);
+struct retrn* builtin_mul(struct args *args);
+struct retrn* builtin_sub(struct args *args);
 
 #endif	/* __H_BUILTINS */
